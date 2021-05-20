c868b88d2e87bd7e3863b41cedfa4ff79e2f2bd6
wtfranco22 <francofranco_96@hotmail.com>
2021-05-20 07:39:51 -0300

    Comenzamos a agregar mas URL para las nuevas funciones

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\BusquedaController;
 use App\Http\Controllers\InscripcionController;
 use App\Http\Controllers\RubroController;
-use App\Models\Busqueda;
-use App\Models\Rubro;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,15 +20,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*---------------------------    R    U    B    R    O    S    -----------------------------*/
+
+Route::get('rubros/crearBusqueda/{idRubro}', [RubroController::class,'crearBusqueda'])->name('rubros.crearBusqueda');
+
+Route::get('rubros/nombre',[RubroController::class,'buscarRubros'])->name('rubros.buscarRubros');
 
 Route::get('rubros/busquedas/{idrubro}', [RubroController::class,'busquedas'])->name('rubros.busquedas');
 
-Route::get('rubros/nombre/{nombre}',[RubroController::class,'buscarRubros'])->name('rubros.nombre');
-
 Route::resource('rubros', RubroController::class);
 
-Route::post('rubros',[RubroController::class,'procesarRubro']);
+
+
+/*---------------------    B    U    S    Q    U    E    D    A    S    --------------------*/
+
+Route::get('busquedas/crearInscripcion/{idBusqueda}', [BusquedaController::class,'crearInscripcion'])->name('busquedas.crearInscripcion');
+
+Route::get('busquedas/inscripciones/{idBusqueda}', [BusquedaController::class,'inscripciones'])->name('busquedas.inscripciones');
 
 Route::resource('busquedas',BusquedaController::class);
+
+
+
+/*------------------  I   N   S   C   R   I   P   C   I   O   N   E   S   ------------------*/
 
 Route::resource('inscripciones', InscripcionController::class);
