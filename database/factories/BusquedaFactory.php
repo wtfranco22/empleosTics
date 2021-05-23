ff805f070d9e479091984c48f55571fa6ef047ae
wtfranco22 <francofranco_96@hotmail.com>
2021-05-23 00:08:33 -0300

    por ultimo del dia de hoy, modificamos la carga de los datos para el poblacional de la base de datos, trabajamos con faker y dejamos comentado tambien las otras posibilidades

--- database/factories/BusquedaFactory.php
+++ database/factories/BusquedaFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Busqueda;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class BusquedaFactory extends Factory
 {
@@ -27,9 +28,11 @@ class BusquedaFactory extends Factory
         //     'titulo'=>$this->faker->jobTitle(),
         //     'descripcion'=>$this->faker->text($maxNbChars=150)
         // ];
+        $nombre=$this->faker->randomElement(['Ticers','Lian y asociados','Stilo','Clinicas Moreno','Exppo','Plomeros Para Usd.']);
         return[
-            'idRubro'=>$this->faker->numberBetween($min=1,$max=50),
-            'empresa'=>$this->faker->randomElement(['Ticers','Lian y asociados','Stilo','Clinicas Moreno','Exppo','Plomeros Para Usd.']),
+            'idRubro'=>$this->faker->numberBetween($min=1,$max=6),
+            //'slug'=>Str::slug($nombre,'-'),
+            'empresa'=>$nombre,
             'titulo'=>$this->faker->randomElement(['Se busca Técnico','Puesto de Abogado Orientado a Comercio','Stilo contrata estilistas','Consultorio de pediatra disponible','Plomero patentado']),
             'descripcion'=>$this->faker->randomElement(['se busca tecnico para mantenimiento de base de datos','forma busca abogado orientado a eyes comerciales','Se solicitan estilistas (F/M) con experiencia','Clinicas Moreno busca pediatras para consultorio de pediatria prenatal','Se busca ingeniero experimentado como consultor de grupo','Se busca plomero patentado para empresa de plomeria'])
         ];
--- database/factories/InscripcionFactory.php
+++ database/factories/InscripcionFactory.php
@@ -27,11 +27,25 @@ class InscripcionFactory extends Factory
         //     'apellido'=>$this->faker->lastName(),
         //     'nombre'=>$this->faker->firstName()
         // ];
+        $personas=[];
+        $personas[]=['nombre'=>'Elsa','apellido'=>'Capunta'];
+        $personas[]=['nombre'=>'Elena','apellido'=>'Nito del Bosque'];
+        $personas[]=['nombre'=>'Aitor','apellido'=>'Tilla'];
+        $personas[]=['nombre'=>'Aitor','apellido'=>'Menta'];
+        $personas[]=['nombre'=>'Aquiles','apellido'=>'Bailo'];
+        $personas[]=['nombre'=>'Zoila','apellido'=>'Mesa'];
+        $personas[]=['nombre'=>'Armando','apellido'=>'Bronca'];
+        $personas[]=['nombre'=>'Pere','apellido'=>'Gil'];
+        $personas[]=['nombre'=>'Ramona','apellido'=>'Ponte Alegra'];
+        $personas[]=['nombre'=>'Flor','apellido'=>'Del Campo'];
+        $personas[]=['nombre'=>'Hess','apellido'=>'Tupido'];
+        $personas[]=['nombre'=>'Jose','apellido'=>'Lomata Feliz'];
+        $indice = $this->faker->numberBetween($min=0,$max=10);
         return [
             'idBusqueda'=>$this->faker->numberBetween($min=1,$max=70),
             'fecha'=>$this->faker->randomElement(['2018-04-10', '2018-04-24', '2018-04-12', '2018-03-06', '2018-04-20', '2018-04-22', '2018-03-02', '2018-04-17', '2018-03-04', '2018-03-06', '2018-03-14', '2018-03-26']),
-            'apellido'=>$this->faker->randomElement([ 'Capunta', 'Nito del Bosque', 'Tilla', 'Menta', 'Bailo', 'Mesa', 'Bronca', 'Pere', 'Ponte Alegre','Del Campo', 'Tupido', 'Lomata Feliz']),
-            'nombre'=>$this->faker->randomElement(['Elsa', 'Elena', 'Aitor', 'Aitor', 'Aquiles', 'Zoila', 'Armando', 'Gil', 'Ramona', 'Flor', 'Hess', 'Jose'])
+            'nombre'=>$personas[$indice]['nombre'],
+            'apellido'=>$personas[$indice]['apellido']
         ];
     }
 }
